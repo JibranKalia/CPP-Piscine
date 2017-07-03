774078372b7142621ae41e060297f3ab994289ea
Jibran Kalia <jkalia@student.42.us.org>
2017-07-03 11:40:14 -0700

    Beginning Hh

--- d00/ex01/rolodex.cpp
+++ d00/ex01/rolodex.cpp
@@ -6,11 +6,12 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 10:16:36 by jkalia            #+#    #+#             */
-/*   Updated: 2017/07/03 11:11:22 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/07/03 11:36:58 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 class Contact {
 	public:
@@ -23,7 +24,7 @@ class Contact {
 		std::string		meal;
 		std::string		color;
 		std::string		secret;
-		int				number;
+		std::string		number;
 		int				date;
 
 };
@@ -31,13 +32,36 @@ class Contact {
 int		main()
 {
 	Contact person;
-
-	std::cout << "Enter the First Name : ";
+	
+	std::cout << "Enter First Name : " << std::endl;
 	std::cin >> person.firstname;
 
-	std::cout << "Enter the Number: ";
+	std::cout << "Enter Last Name : " << std::endl;
+	std::cin >> person.lastname;
+
+	std::cin.ignore();
+	std::cout << "Enter Nickname: " << std::endl;
+	std::getline(std::cin, person.nickname);
+
+	std::cin.ignore();
+	std::cout << "Enter Phone Number: " << std::endl;
+	std::getline(std::cin, person.number);
+
+	std::cout << "\n\nFirst Name:\t" << person.firstname;
+	std::cout << "\nLast Name:\t" << person.lastname;
+	std::cout << "\nNick Name:\t" << person.nickname;
+	std::cout << "\nPhone Number:\t" << person.number << std::endl;
+
+	/**
+
+	std::cout << "Enter the Number: " << std::endl;
 	std::cin >> person.number;
 
-	std::cout << person.firstname << ": " << person.number << std::endl;
+	std::cin.ignore();
+	std::cout << "Enter Nickname: ";
+	std::getline(std::cin, person.nickname);
+
+	std::cout << person.firstname << ": " << person.nickname << std::endl;
+	**/
 	return (0);
 }
