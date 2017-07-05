272a42e395d8fcd84bb718260a7897d188dbf90f
Jibran Kalia <jkalia@student.42.us.org>
2017-07-05 10:58:37 -0700

    Beginning work on ex03

---
+++ ex03/ZombieHorde.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ZombieHorde.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/05 10:54:19 by jkalia            #+#    #+#             */
+/*   Updated: 2017/07/05 10:57:58 by jkalia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIEHORDE_HPP
+#define ZOMBIEHORDE_HPP
+
+#include "Zombie.hpp"
+
+class ZombieHorde {
+ public:
+  ZombieHorde(void);
+  ~ZombieHorde(void);
+
+ private:
+};
+
+#endif
