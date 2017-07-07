93d869624fca8f5cea825a545a087a12a38b6adc
Jibran Kalia <jkalia@student.42.us.org>
2017-07-06 17:03:02 -0700

    Google format for ex01

--- ex02/Fixed.class.cpp
+++ ex02/Fixed.class.cpp
@@ -19,11 +19,13 @@ Fixed::Fixed() : _rawbits(0) {
   std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const int n) : _rawbits((long)n << _nfracbits) {
+Fixed::Fixed(const int n) : _rawbits((long) n << _nfracbits) {
   std::cout << "Int constructor called" << std::endl;
 }
 
-Fixed::Fixed(const float f) : _rawbits(roundf((f * (1 << _nfracbits)))) {
+Fixed::Fixed(const float f) : _rawbits(
+	roundf((f * (1 << _nfracbits)))
+){
   std::cout << "Float constructor called" << std::endl;
 }
 
@@ -36,21 +38,29 @@ Fixed::Fixed(const Fixed& src) {
 
 Fixed& Fixed::operator=(const Fixed& rhs) {
   std::cout << "Assignation operator called" << std::endl;
-  if (this != &rhs) _rawbits = rhs.getRawBits();
+  if (this != &rhs)
+	  _rawbits = rhs.getRawBits();
   return *this;
 }
 
-std::ostream& operator<<(std::ostream& out, const Fixed& f) {
-  out << f.toFloat();
-  return out;
+std::ostream& operator<<(std::ostream &out, const Fixed &f)
+{
+	out << f.toFloat();
+	return out;
 }
 
-int Fixed::getRawBits() const { return _rawbits; }
+int Fixed::getRawBits() const {
+  return _rawbits;
+}
 
 void Fixed::setRawBits(int const raw) { _rawbits = raw; }
 
-float Fixed::toFloat() const {
-  return (float)(_rawbits / (float)(1 << _nfracbits));
+float	Fixed::toFloat() const
+{
+	return (float)(_rawbits / (float)(1 << _nfracbits));
 }
 
-int Fixed::toInt() const { return (_rawbits >> _nfracbits); }
+int Fixed::toInt() const
+{
+	return (_rawbits >> _nfracbits);
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,24 +6,22 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 13:32:59 by jkalia            #+#    #+#             */
-/*   Updated: 2017/07/06 15:40:40 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/07/06 17:01:22 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.class.hpp"
 
-int main(void) {
-  Fixed a;
-  Fixed const b(10);
-  Fixed const c(42.42f);
-  Fixed const d(b);
-  a = Fixed(1234.4321f);
-  std::cout << "a is " << a << std::endl;
-  std::cout << "b is " << b << std::endl;
-  std::cout << "c is " << c << std::endl;
-  std::cout << "d is " << d << std::endl;
-  std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-  std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-  std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-  std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+int main( void )
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
 }
